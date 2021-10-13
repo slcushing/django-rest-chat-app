259d640cfeb0286b73e65ca8de8679be91808203
Stephanie Cushing <slcushing@gmail.com>
2021-10-13 14:12:37 -0400

    Added Superuser & serializer for Room

--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
+
+class RoomListAPIView(generics.ListCreateAPIView): #GET & POST
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+# class MessageListAPIView(generics.ListCreateAPIView): #GET & POST
